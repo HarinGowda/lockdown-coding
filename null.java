6d8278e0d94104598d2823f23c3477491ebb0412
sharingowda <53472857+sharingowda@users.noreply.github.com>
2020-07-16 01:29:41 -1200

    Create null.java

---
+++ null.java
@@ -0,0 +1,22 @@
+#include<stdio.h>
+unsigned int reverseBits(unsigned int n) 
+{ 
+	unsigned int rev = 0; 
+		while (n > 0) 
+	{ 
+		rev <<= 1; 
+		if (n & 1 == 1) 
+			rev ^= 1; 
+		n >>= 1; 
+			
+	} 
+	return rev; 
+} 
+int main() 
+{ 
+	unsigned int n;
+	printf("input: ");
+	scanf("%d",&n);
+	printf("output: %d\n", reverseBits(n)); 
+	return 0; 
+}
